a26d160329356c65ccbc818055bfeaa7b5940f04
Rodion Brodetsky <s7ang3r@gmail.com>
2010-08-21 20:11:29 +0300

    modified:   pytripcode.py

--- pytripcode.py
+++ pytripcode.py
@@ -7,13 +7,28 @@ import re
 import crypt
 
 def MakeTripCode (expression):
-    expression = expression.decode('utf8','ignore')
+    expression = expression.decode('utf8','ignore')\
+        .encode('shift_jis','ignore')\
+        .replace("'",'')\
+        .replace(',',',')\
+        .replace('<','&lt;')\
+        .replace('>','&gt;')\
+        .replace('"','&quot;')
+    e_expression = (expression + '...')[1:3]
+    e_expression = re.compile('[^\.-z]').sub('.', e_expression)
+    e_expression = e_expression.translate(string.maketrans(':;<=>?@[\\]^_`', 'ABCDEFGabcdef'))
+    tripcode = crypt.crypt(expression, e_expression)[-10:]
     return tripcode
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print "Program requires arguments."
         sys.exit(0)
+
+    try:
+        iterator = int(sys.argv[2])
+    except IndexError:
+        iterator = 0
 
     while True:
         if re.search(sys.argv[1],string.lower(MakeTripCode(str(iterator))))>-1:
