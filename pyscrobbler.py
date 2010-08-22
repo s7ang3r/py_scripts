477b0a578c99406ea288fcee643dda78ed0087cb
Rodion Brodetsky <s7ang3r@gmail.com>
2010-08-22 16:19:34 +0300

    modified:   pyscrobbler.py

--- pyscrobbler.py
+++ pyscrobbler.py
@@ -2,17 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import time
-import os.path
+import os
 import hashlib
 import urllib
 import httplib
 import urlparse
 
+LOGIN = "login"
+PASSWORD = "password"
 CHECK_LINE = "#AUDIOSCROBBLER/1.1\n"
 
 def ParseLog(filename):
     try:
-        logfile = open(filename,'r')
+        logfile = open( filename , 'r' )
     except IOError:
        print "Cant open file %s" % logfile
        quit()
@@ -20,15 +22,17 @@ def ParseLog(filename):
         print "Unknown file format"
         logfile.close()
         quit()
-    return 0
 
 def CreateSession():
+    timestamp = int(time.time())
+    token = hashlib.md5(PASSWORD + str(timestamp)).hexdigest()
+    connection = httplib.HTTPConnection("post.audioscrobbler.com") 
     return 0;
 
 def Scrobble():
     return 0;
 
 if __name__ == "__main__":
-    #ParseLog(".scrobbler.log")
+    ParseLog(".scrobbler.log")
     CreateSession()
     Scrobble()
