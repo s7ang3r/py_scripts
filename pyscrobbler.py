ba3682adccebf39267e3379504d24fdffa8b1cf9
Rodion Brodetsky <s7ang3r@gmail.com>
2010-08-28 10:01:16 +0300

    modified:   pyscrobbler.py

--- pyscrobbler.py
+++ pyscrobbler.py
@@ -11,8 +11,8 @@ import urlparse
 FILENAME = ".scrobbler.log"
 LOGIN = ""
 PASSWORD = ""
-APP_NAME = "" #Register new application!!!
-APP_VERSION = "1.0"
+APP_NAME = "qts" #Register new application!!!
+APP_VERSION = "0.11"
 TZ = 0
 
 def Scrobble(filename):
@@ -39,26 +39,26 @@ def Scrobble(filename):
     submission_path = url.path
     session_id = data[1]
     for line in filename.xreadlines():
-        data = line.split("\t")
-        print
-        if (data[5] == "L"):
-            params = urllib.urlencode({'s': session_id,\
-                    'a[0]': data[0],\
-                    't[0]': data[2],\
-                    'i[0]': int(data[6])+timedelay,\
-                    'o[0]': 'P',\
-                    'r[0]': '',\
-                    'l[0]': data[4],\
-                    'b[0]': data[1],\
-                    'n[0]': data[3],\
-                    'm[0]': data[7]})
-            connection.request("POST", submission_path, params, {"Content-type": "application/x-www-form-urlencoded"})
-            response = conn.getresponse().read()[:-1]
-            if (response == "OK"):
-                success+=1
-            else:
-                failure+=1
-            print "%s - %s [%s]" % (data[0], data[2], response)
+        if not line.startswith('#'):
+            data = line.split("\t")
+            if (data[5] == "L"):
+                params = urllib.urlencode({'s': session_id,\
+                                           'a[0]': data[0],\
+                                           't[0]': data[2],\
+                                           'i[0]': int(data[6])+timedelay,\
+                                           'o[0]': 'P',\
+                                           'r[0]': '',\
+                                           'l[0]': data[4],\
+                                           'b[0]': data[1],\
+                                           'n[0]': data[3],\
+                                           'm[0]': data[7]})
+                connection.request("POST", submission_path, params,{"Content-type": "application/x-www-form-urlencoded"})
+                response = conn.getresponse().read()[:-1]
+                if (response == "OK"):
+                    success+=1
+                else:
+                    failure+=1
+                print "%s - %s [%s]" % (data[0], data[2], response)
     connection.close();
     filename.close()
     print "%i tracks submitted.\n%i failed" % (success, failure)
