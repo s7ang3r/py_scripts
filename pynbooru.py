ca8a5654d1bf2bd90733bbb0e26a697741092c10
Rodion Brodetsky <s7ang3r@gmail.com>
2010-08-29 17:40:46 +0300

    modified:   pynbooru.py

--- pynbooru.py
+++ pynbooru.py
@@ -10,3 +10,13 @@ HOST = 'danbooru.donmai.us'
 URL = '/post/index.xml'
 LIMIT = 1000
 
+def Download(url):
+    return 0
+
+def FetchIndex(limit, page):
+    return 0
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "Usage: %s tag" % sys.argv[0]
+        exit(1)
