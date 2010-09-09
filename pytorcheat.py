578468a6070f543c7c2196e2a269e9d26d08c5bd
Rodion Brodetsky <s7ang3r@gmail.com>
2010-09-09 13:03:07 +0300

    modified:   pytorcheat.py

--- pytorcheat.py
+++ pytorcheat.py
@@ -24,61 +24,61 @@ __author__ = "Rodion Brodetsky"
 __license__ = "GPL"
 __version__ = "0.0.5"
 
-def ReadTorrent(filename):
-    return bencode.bdecode(open(filename).read())
+def ReadTorrent( filename ):
+    return bencode.bdecode( open( filename ).read() )
 
-def GetInfoHash(torrent_info):
-    return hashlib.sha1(bencode.bencode(torrent_info)).digest()
+def GetInfoHash( torrent_info ):
+    return hashlib.sha1( bencode.bencode( torrent_info ) ).digest()
 
-def GetFileSize(torrent_info):
+def GetFileSize( torrent_info ):
     try:
         return torrent_info['length']
     except KeyError:
-        return sum(file['length'] for file in torrent_info['files'])
+        return sum( file['length'] for file in torrent_info['files'] )
 
 def MakePeerId():
-    num_random_chars = 20 - len(PEER_ID_PREFIX)
+    num_random_chars = 20 - len( PEER_ID_PREFIX )
     alphabet = string.ascii_letters + string.digits
-    random_string = ''.join(random.choice(alphabet) for i in range(num_random_chars))
+    random_string = ''.join( random.choice( alphabet ) for i in range( num_random_chars ) )
     return PEER_ID_PREFIX + random_string
 
-def MakeAnnounceUrl(torrent_data, peer_id, uploaded):
+def MakeAnnounceUrl( torrent_data, peer_id, uploaded ):
     base_url = torrent_data['announce']
-    scheme, netloc, path, base_query, fragment = urlparse.urlsplit(base_url)
-    base_query_list = cgi.parse_qsl(base_query)
+    scheme, netloc, path, base_query, fragment = urlparse.urlsplit( base_url )
+    base_query_list = cgi.parse_qsl( base_query )
     query_list = base_query_list + [
-            ('info_hash', GetInfoHash(torrent_data['info'])),
-            ('peer_id', peer_id),
-            ('port', PORT),
-            ('uploaded', uploaded),
-            ('downloaded', GetFileSize(torrent_data['info'])),
-            ('left', 0),
+            ( 'info_hash', GetInfoHash( torrent_data['info'] ) ),
+            ( 'peer_id', peer_id ),
+            ( 'port', PORT ),
+            ( 'uploaded', uploaded ),
+            ( 'downloaded', GetFileSize( torrent_data['info'] ) ),
+            ( 'left', 0 ),
     ]
-    query = urllib.urlencode(query_list)
-    return urlparse.urlunsplit((scheme, netloc, path, query, fragment))
+    query = urllib.urlencode( query_list )
+    return urlparse.urlunsplit( ( scheme, netloc, path, query, fragment ) )
 
-def FakeUpload(torrent_data):
+def FakeUpload( torrent_data ):
     peer_id = MakePeerId()
     uploaded = 0
     while True:
-        url = MakeAnnounceUrl(torrent_data, peer_id, uploaded)
-        request = urllib2.Request(url, None, {'User-Agent': USER_AGENT})
-        response = bencode.bdecode(urllib2.urlopen(request).read())
+        url = MakeAnnounceUrl( torrent_data, peer_id, uploaded )
+        request = urllib2.Request( url, None, {'User-Agent': USER_AGENT} )
+        response = bencode.bdecode( urllib2.urlopen( request ).read() )
         if 'failure' in response:
             print 'Announce failed: %s' % response['failure']
-            time.sleep(60)
+            time.sleep( 60 )
         else:
             interval = response['interval']
-            will_sleep_until = (datetime.datetime.now() + datetime.timedelta(seconds=interval)).strftime('%H:%M:%S')
-            print 'Uploaded %s bytes, next request at %s.' % (uploaded, will_sleep_until)
+            will_sleep_until = ( datetime.datetime.now() + datetime.timedelta( seconds = interval ) ).strftime( '%H:%M:%S' )
+            print 'Uploaded %s bytes, next request at %s.' % ( uploaded, will_sleep_until )
             uploaded += UPLOAD_SPEED * interval
-            time.sleep(interval)
+            time.sleep( interval )
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len( sys.argv ) < 2:
         print 'Usage: %s <filename.torrent> or several torrents' % sys.argv[0]
-        sys.exit(1)
+        sys.exit( 1 )
     for torrent in sys.argv[1:]:
-        torrent_data = ReadTorrent(torrent)
-        thread = threading.Thread(target=FakeUpload, args=(torrent_data,))
+        torrent_data = ReadTorrent( torrent )
+        thread = threading.Thread( target = FakeUpload, args = ( torrent_data, ) )
         thread.start()
