72d88a16d75823fa5750f8943216d1e46105e3c8
Rodion Brodetsky <s7ang3r@gmail.com>
2010-09-20 17:45:22 +0300

    modified:   pynbooru.py

--- pynbooru.py
+++ pynbooru.py
@@ -27,9 +27,9 @@ def MakeDir(dir):
 
 def Download(url, path):
     image = url.split("/")[-1]
-    print "Downloading %s" % url
+    print "[+] Downloading %s" % url
     urllib.urlretrieve(url, path + image)
-    print "Downloading of %s complete" % image
+    print "[+] Downloading of %s complete" % image
 
 
 def FetchIndex(limit, page):
@@ -40,14 +40,14 @@ def FetchIndex(limit, page):
     connection.request('GET', URL + '?' + args)
     response = connection.getresponse()
     if response.status != 200:
-        print 'Unable to fetch index: HTTP%d' % response.status
+        print '[-] Unable to fetch index: HTTP%d' % response.status
         exit(1)
     return response.read()
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "Usage: %s tag" % sys.argv[0]
+        print "[?] Usage: %s tag" % sys.argv[0]
         exit(1)
     try:
         dirname = os.path.abspath(sys.argv[1])
@@ -58,14 +58,14 @@ if __name__ == "__main__":
     try:
         count = int(re.findall('<posts count="([0-9]+)"', data)[0])
     except:
-        print "Could not locate number of posts which match this tag."
+        print "[-] Could not locate number of posts which match this tag."
         exit(1)
     if count > LIMIT:
         for page in range(2, count / LIMIT + 2):
             data += FetchIndex(LIMIT, page)
     imgs = re.findall('file_url="([^"]+)"', data)
-    print "Found %s images by tag: %s." % (len(imgs), sys.argv[1])
-    print "Starting download."
+    print "[+] Found %s images by tag: %s." % (len(imgs), sys.argv[1])
+    print "[+] Starting download."
     for img in imgs:
         thread = threading.Thread(target=Download,\
                                   args=(img, dirname + '/'))
