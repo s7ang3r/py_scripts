04c0ffa8474ff219224d40d3d649516383f39ff6
Rodion Brodetsky <s7ang3r@gmail.com>
2010-12-04 18:58:30 +0200

    modified:   pycompare/pycompare.py

--- pycompare/pycompare.py
+++ pycompare/pycompare.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python -O
 # -*- coding: utf-8 -*-
 
-import md5
+import hashlib
 import os
 import sys
 
 
 def Md5File(filename):
     fh = open(filename)
-    digest = md5.new()
+    digest = hashlib.new('md5')
     while 1:
         buf = fh.read(4096)
         if buf == "":
