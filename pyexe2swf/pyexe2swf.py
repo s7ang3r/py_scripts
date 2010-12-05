29c2743829e155838e0c158895c9978fe552dcc5
Rodion Brodetsky <s7ang3r@gmail.com>
2010-12-05 15:50:46 +0200

    modified:   pyexe2swf/pyexe2swf.py

---
+++ pyexe2swf/pyexe2swf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python -O
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+def SwfPosition(filename):
+    pass
+
+
+def ExtractSwf(filein, fileout, position):
+    pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print 'Usage: %s file.exe' % sys.argv[0]
+        sys.exit(1)
+    file = open(sys.argv[1], "rb+")
+    position = SwfPosition(file)
