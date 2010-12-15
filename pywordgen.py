d93af5d4106c9d83f6d764a00e0382141ce7708c
Rodion Brodetsky <s7ang3r@gmail.com>
2010-12-15 17:44:51 +0200

    modified:   pywordgen.py

--- pywordgen.py
+++ pywordgen.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python -O
 # -*- coding: utf-8 -*-
 
+import optparse
 import os
 import time
+
+
+def ParseArgs():
+    parser = optparse.OptionParser(usage="%prog [options] ",\
+                                 version="%prog 0.3")
 
 
 if __name__ == "__main__":
