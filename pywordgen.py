4d0cd5c78f6c2349ae4231b50c4d1260fbadfb88
Rodion Brodetsky <s7ang3r@gmail.com>
2010-12-17 18:18:26 +0200

    modified:   pywordgen.py

--- pywordgen.py
+++ pywordgen.py
@@ -10,31 +10,40 @@ def ParseArgs():
                                  version="%prog 0.3")
     parser.add_option('--numbers',\
                       dest='numb',\
-                      help="Numbers")
+                      help="Numbers",\
+                      default=False)
     parser.add_option('--capitals',\
                       dest='caps',\
-                      help="Capital Letters")
+                      help="Capital Letters",\
+                      default=False)
     parser.add_option('--lowercase',\
                       dest='lows',\
-                      help="Lowercase Letters")
+                      help="Lowercase Letters",\
+                      default=False)
     parser.add_option('--numcaps',\
                       dest='numcaps',\
-                      help="Numbers + Capital Letters")
+                      help="Numbers + Capital Letters",\
+                      default=False)
     parser.add_option('--numlow',\
                       dest='numlowlows',\
-                      help="Numbers + Lowercase Letters")
+                      help="Numbers + Lowercase Letters",\
+                      default=False)
     parser.add_option('--numcapslow',\
                       dest='numcapslow',\
-                      help="Numbers + Capital Letters + Lowercase Letters")
+                      help="Numbers + Capital Letters + Lowercase Letters",\
+                      default=False)
     parser.add_option('--capslow',\
                       dest='capslow',\
-                      help="Capital Letters + Lowercase Letters")
+                      help="Capital Letters + Lowercase Letters",\
+                      default=False)
     parser.add_option('--min',\
                       dest='min',\
-                      help="Minimum size of the word")
+                      help="Minimum size of the word",\
+                      default=0)
     parser.add_option('--max',\
                       dest='max',\
-                      help="Maximum size of the word")
+                      help="Maximum size of the word",\
+                      default=5)
     optparse.IndentedHelpFormatter().set_long_opt_delimiter = 'z'
     (options, args) = parser.parse_args()
     if not args:
