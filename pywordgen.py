cf99c9798915d318bf3bb4c81c55dc304cafffcd
Rodion Brodetsky <s7ang3r@gmail.com>
2010-12-17 20:41:32 +0200

    modified:   pywordgen.py

--- pywordgen.py
+++ pywordgen.py
@@ -9,33 +9,33 @@ def ParseArgs():
     parser = optparse.OptionParser(usage="%prog [options] ",\
                                  version="%prog 0.3")
     parser.add_option('--numbers',\
-                      dest='numb',\
-                      help="Numbers",\
-                      default=False)
+                      action="store_true",\
+                      dest='numbs',\
+                      help="Numbers")
     parser.add_option('--capitals',\
+                      action="store_true",\
                       dest='caps',\
-                      help="Capital Letters",\
-                      default=False)
+                      help="Capital Letters")
     parser.add_option('--lowercase',\
+                      action="store_true",\
                       dest='lows',\
-                      help="Lowercase Letters",\
-                      default=False)
+                      help="Lowercase Letters")
     parser.add_option('--numcaps',\
+                      action="store_true",\
                       dest='numcaps',\
-                      help="Numbers + Capital Letters",\
-                      default=False)
+                      help="Numbers + Capital Letters")
     parser.add_option('--numlow',\
-                      dest='numlowlows',\
-                      help="Numbers + Lowercase Letters",\
-                      default=False)
+                      action="store_true",\
+                      dest='numlow',\
+                      help="Numbers + Lowercase Letters")
     parser.add_option('--numcapslow',\
+                      action="store_true",\
                       dest='numcapslow',\
-                      help="Numbers + Capital Letters + Lowercase Letters",\
-                      default=False)
+                      help="Numbers + Capital Letters + Lowercase Letters")
     parser.add_option('--capslow',\
+                      action="store_true",\
                       dest='capslow',\
-                      help="Capital Letters + Lowercase Letters",\
-                      default=False)
+                      help="Capital Letters + Lowercase Letters")
     parser.add_option('--min',\
                       dest='min',\
                       help="Minimum size of the word",\
@@ -46,9 +46,6 @@ def ParseArgs():
                       default=5)
     optparse.IndentedHelpFormatter().set_long_opt_delimiter = 'z'
     (options, args) = parser.parse_args()
-    if not args:
-        parser.print_help()
-        exit(1)
     return (options, args)
 
 
@@ -59,3 +56,32 @@ if __name__ == "__main__":
     lows = range(97, 123)
     numFile = 0
     f = open('wordlist-' + str(numFile) + '.txt', 'w')
+    minimum = int(options.min)
+    maximum = int(options.max)
+    poss = []
+    if options.numbs:
+        poss += nums
+        print "numb"
+    elif options.caps:
+        poss += caps
+        print "caps"
+    elif options.lows:
+        poss += lows
+        print "lows"
+    elif options.numcaps:
+        poss += nums
+        poss += caps
+        print "numscaps"
+    elif options.numlow:
+        poss += nums
+        poss += lows
+        print "numslows"
+    elif options.numcapslow:
+        poss += nums
+        poss += caps
+        poss += lows
+        print "numcapslow"
+    elif options.capslow:
+        poss += caps
+        poss += lows
+        print "capslow"
