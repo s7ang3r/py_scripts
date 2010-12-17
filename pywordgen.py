81dace73efca1b6fb48f4f4db0d18d88eeb19d51
Rodion Brodetsky <s7ang3r@gmail.com>
2010-12-17 15:47:22 +0200

    modified:   pywordgen.py

--- pywordgen.py
+++ pywordgen.py
@@ -8,9 +8,16 @@ import os
 def ParseArgs():
     parser = optparse.OptionParser(usage="%prog [options] ",\
                                  version="%prog 0.3")
+    optparse.IndentedHelpFormatter().set_long_opt_delimiter = 'z'
+    (options, args) = parser.parse_args()
+    if not args:
+        parser.print_help()
+        exit(1)
+    return (options, args)
 
 
 if __name__ == "__main__":
+    (options, tags) = ParseArgs()
     nums = range(48,58)
     caps = range(65,91)
     lows = range(97,123)
